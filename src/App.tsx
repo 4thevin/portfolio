b0f19c6f02906b018cee6ecb93fd830564fb6616
4thevin <vineethforegard@gmail.com>
2025-04-02 16:01:23 -0400

    Initial portfolio setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Box, Typography} from "@mui/material";
+
+function App() {
+
+    return (
+        <Box>
+            <Typography variant={"h1"}>
+                Vin Foregard
+            </Typography>
+        </Box>
+    )
+}
+
+export default App
